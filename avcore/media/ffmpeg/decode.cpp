87a76a3f1e05ed64b9612e9dd5e874a90ed1ff00
yangkun <yang1001yk@gmail.com>
2021-06-26 22:55:15 +0800

    音频视频操作完成

--- avcore/media/ffmpeg/decode.cpp
+++ avcore/media/ffmpeg/decode.cpp
@@ -13,7 +13,8 @@ int main(int argc, char *argv[]) {
 
 //    const char * inpath = argv[1];
 //    const char * outpath = argv[2];
-    const char *inpath = "https://clipres.yishihui.com/longvideo/material/video/vpc/20210624/186190163e976bf7ce340ad9b7903d039ed88111624472258741";
+//    const char *inpath = "https://clipres.yishihui.com/test/noBFrame.mp4";
+    const char *inpath = "/Users/devyk/Data/Project/sample/github_code/YKAVStudyPlatform/temp/clip.mp4";
     const char *outpath = "/Users/devyk/Downloads/0.mp4";
 
     //初始化网络库(可以打开 rtmp、rtsp、http 等协议的流媒体体视频)
@@ -38,7 +39,6 @@ int main(int argc, char *argv[]) {
         char buf[1024] = {0};
         av_strerror(re, buf, sizeof(buf) - 1);
         cout << "open " << inpath << " failed! :" << buf << endl;
-        getchar();
         return -1;
     }
     cout << "open " << inpath << " success! " << endl;
@@ -94,7 +94,6 @@ int main(int argc, char *argv[]) {
     AVCodec *vcodec = avcodec_find_decoder(ic->streams[videoStream]->codecpar->codec_id);
     if (!vcodec) {
         cout << "can't find the codec id" << ic->streams[videoStream]->codecpar->codec_id << endl;
-        getchar();
         return -1;
     }
     cout << "find video the AVCodec id = " << ic->streams[videoStream]->codecpar->codec_id << endl;
@@ -111,7 +110,6 @@ int main(int argc, char *argv[]) {
         char buf[1024] = {0};
         av_strerror(re, buf, sizeof(buf) - 1);
         cout << "video avcodec_open2 failed!" << buf << endl;
-        getchar();
         return -1;
     }
     cout << "video avcodec_open2 success!" << endl;
@@ -120,7 +118,6 @@ int main(int argc, char *argv[]) {
     AVCodec *acodec = avcodec_find_decoder(ic->streams[audioStream]->codecpar->codec_id);
     if (!acodec) {
         cout << "can't find the codec id" << ic->streams[audioStream]->codecpar->codec_id << endl;
-        getchar();
         return -1;
     }
     cout << "find audio the AVCodec id = " << ic->streams[audioStream]->codecpar->codec_id << endl;
@@ -137,7 +134,6 @@ int main(int argc, char *argv[]) {
         char buf[1024] = {0};
         av_strerror(re, buf, sizeof(buf) - 1);
         cout << "audio avcodec_open2 failed!" << buf << endl;
-        getchar();
         return -1;
     }
     cout << "audio avcodec_open2 success!" << endl;
@@ -149,17 +145,45 @@ int main(int argc, char *argv[]) {
 
     //像素格式转换上下文
     SwsContext *vsctx = NULL;
-
+    //原始像素格式转换为 rgb 的存储
+    unsigned char *rgb = NULL;
+    //音频重采样
+    SwrContext *asctx = swr_alloc();
+    //设置重采样参数
+    asctx = swr_alloc_set_opts(asctx //重采样上下文
+            , av_get_default_channel_layout(2)//输出声道格式
+            , AV_SAMPLE_FMT_S16 //输出声音样本格式
+            , 48000 //输出采样率
+            , av_get_default_channel_layout(actx->channels)//输入通道数
+            , actx->sample_fmt                            //输入声音样本格式
+            , actx->sample_rate, 0, 0  //输入音频采样率
+    );
+    //初始化采样上下文
+    re = swr_init(asctx);
+    if (re != 0) {
+        char buf[1024] = {0};
+        av_strerror(re, buf, sizeof(buf) - 1);
+        cout << "audio swr_init failed!" << buf << endl;
+        return -1;
+    }
+    //重采样之后存入的数据
+    unsigned char *pcm = NULL;
     for (;;) {
         int re = av_read_frame(ic, pkt);
         if (re != 0) {
             //循环播放
             cout << "==============================end==============================" << endl;
-//            int ms = 3000; //三秒位置 根据时间基数（分数）转换
-//            long long pos = (double) ms / (double) 1000 * r2d(ic->streams[pkt->stream_index]->time_base);
-//            av_seek_frame(ic, videoStream, pos, AVSEEK_FLAG_BACKWARD | AVSEEK_FLAG_FRAME);
-//            continue;
-            break;
+            cout << "==============================end==============================" << endl;
+            cout << "==============================end==============================" << endl;
+//          YSleep(3000);
+            int ms = 3000; //三秒位置 根据时间基数（分数）转换
+            long long pos = (double) ms / (double) 1000 / r2d(ic->streams[videoStream]->time_base);
+            re = av_seek_frame(ic, videoStream, pos, AVSEEK_FLAG_BACKWARD | AVSEEK_FLAG_FRAME);
+            if (re >= 0) {
+                cout << "av_seek_frame success   " << endl;
+            }
+            continue;
+//            break;
         }
 
         cout << "pkt->size = " << pkt->size << endl;
@@ -231,41 +255,101 @@ int main(int argc, char *argv[]) {
             }
 
             if (avcc == vctx) {
+                const int in_width = frame->width;
+                const int in_height = frame->height;
+                const int out_width = in_width / 2;
+                const int out_height = in_height / 2;
+                /**
+                 * @param context : 缩放上下文，如果为 NULL,那么内部会进行创建，
+                 * 如果已经存在，参数也没有发生变化，那么就直接返回当前，否者释放缩放上下文，重新创建。
+                 * @param srcW : 输入的宽
+                 * @param srcH : 输入的高
+                 * @param srcFormat : 输入的格式
+                 * @param dstW : 输出的宽
+                 * @param dstH : 输出的高
+                 * @param dstFormat : 输出的格式
+                 * @param flags : 提供了一系列的算法，快速线性，差值，矩阵，不同的算法性能也不同，快速线性算法性能相对较高。只针对尺寸的变换。
+                 * @param srcFilter : 输入过滤器
+                 * @param dstFilter : 输出过滤器
+                 * @param param : 这个跟 flags 算法相关，一般传入 O
+                 */
                 vsctx = sws_getCachedContext(
                         vsctx,//传入NULL 会新创建
-                        frame->width, frame->height, (AVPixelFormat) frame->format, //输入的宽高，格式
-                        frame->width, frame->height, AV_PIX_FMT_RGBA, //输出的宽高，格式
+                        in_width, in_height, (AVPixelFormat) frame->format, //输入的宽高，格式
+                        out_width, out_height, AV_PIX_FMT_RGBA, //输出的宽高，格式
                         SWS_BILINEAR, //尺寸变换的算法
                         0, 0, 0
                 );
-
                 if (vsctx) {
                     cout << "像素格式尺寸转换上下文创建或者获取成功！" << endl;
+                    cout << "in_width=" << in_width << endl;
+                    cout << "in_height=" << in_height << endl;
+                    cout << "out_width=" << out_width << endl;
+                    cout << "out_height=" << out_height << endl;
+                    if (!rgb) rgb = new unsigned char[out_width * out_height * 4];
+                    uint *data[2] = {0};
+                    data[0] = (uint *) rgb;
+                    data[1] = NULL;
+                    int lines[2] = {0};
+                    lines[0] = out_width * 4;
+                    lines[1] = NULL;
+                    /**
+                     * @param c         缩放上下文
+                     * @param srcSlice  YUV 切换数据可以是指针，也可以是数组
+                     * @param srcStride 对应 YUV 一行的大小
+                     * @param srcSliceY 这个用不到传入 0 即可
+                     * @param srcSliceH YUV 的高
+                     * @param dst       输出的像素格式数据
+                     * @param dstStride 输出的像素格式数据的大小
+                     * @return          返回转换后的高
+                     */
+                    //the height of the output slice
+                    re = sws_scale(vsctx,
+                                   frame->data, //输入数据
+                                   frame->linesize,//输入行大小
+                                   0,
+                                   frame->height,//输出高度
+                                   (uint8_t *const *) (data), //输出数据
+                                   lines//输出大小
+                    );
+                    cout << "sws_scale success! return height of the output slice =" << re << endl;
                 } else {
                     cout << "像素格式尺寸转换上下文创建或者获取失败！" << endl;
                 }
+            } else if (avcc == actx) {//音频
+                uint8_t *data[2] = {0};
+                if (!pcm) pcm = new uint8_t[frame->nb_samples * 16 / 8 * 2];
+                data[0] = {pcm};
+                int len = swr_convert(asctx, data //输出的数据
+                        , frame->nb_samples //输出
+                        , (const uint8_t **) frame->data //输入的音频数据
+                        , frame->nb_samples //输入
+                );
+                if (len >= 0) {
+                    cout << "swr_convert success return len = " << len << endl;
+                } else {
+                    cout << "swr_convert failed return len = " << len << endl;
+                }
             }
         }
-
-
-
-
 
         //释放，引用计数-1 为0释放空间
         av_frame_unref(frame);
 //        YSleep(20);
     }
+    if (vsctx)sws_freeContext(vsctx);
+    if (asctx)swr_close(asctx);
+    if (asctx)swr_free(&asctx);
+    if (pkt)av_packet_free(&pkt);
+    if (frame)av_frame_free(&frame);
+    //释放封装上下文，并且把ic置0
+    if (ic) avformat_close_input(&ic);
 
-    av_packet_free(&pkt);
-    av_frame_free(&frame);
-
-
-    if (ic) {
-        //释放封装上下文，并且把ic置0
-        avformat_close_input(&ic);
-    }
-
-    getchar();
+    vsctx = NULL;
+    asctx = NULL;
+    pkt = NULL;
+    frame = NULL;
+    ic = NULL;
     return 1;
 }
 
--- avcore/media/ffmpeg/decode.h
+++ avcore/media/ffmpeg/decode.h
@@ -4,19 +4,21 @@
 
 #ifndef YKAVSTUDYPLATFORM_DECODE_H
 #define YKAVSTUDYPLATFORM_DECODE_H
+
 #include <iostream>
 #include <thread>
+
 extern "C" {
 #include "libavcodec/avcodec.h"
 #include "libavutil/avutil.h"
 #include "libavformat/avformat.h"
 #include "libswscale/swscale.h"
+#include "libswresample/swresample.h"
 }
 using namespace std;
 
-static double r2d(AVRational r)
-{
-    return r.den == 0 ? 0:(double)r.num / (double)r.den;
+static double r2d(AVRational r) {
+    return r.den == 0 ? 0 : (double) r.num / (double) r.den;
 }
 
 /**
@@ -24,7 +26,7 @@ static double r2d(AVRational r)
  * @param ms
  */
 void YSleep(long ms) {
-    chrono::milliseconds  du(ms);
+    chrono::milliseconds du(ms);
     this_thread::sleep_for(du);
 }
 
